da85b9db868634b3477be6d1ccd5fcebf50496c0
Mephisto00 <Martijn-hulshof@hotmail.com>
2017-05-09 15:03:52 +0200

    navigatiebar

--- Application/NavBar.php
+++ Application/NavBar.php
@@ -1,15 +1,17 @@
+
 <?php include("header.php");?>
 
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">Euratex</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      <li><a href="#">Bedrijf & Project</a></li>
+      <li><a href="#">Aspecten & Effecten</a></li>
+      <li><a href="#">Rapportages</a></li>
+      <li><a href="#">Beheer</a></li>
     </ul>
   </div>
 </nav>
