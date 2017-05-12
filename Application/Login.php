04a3065e9bb343f81c4bdcf1164eaeb17f64d848
Ali Yilmaz <aliyilmazgc@gmail.com>
2017-05-12 14:23:46 +0200

    Loginpagina aangepast

--- Application/Login.php
+++ Application/Login.php
@@ -2,7 +2,7 @@
 
 <div class="container">
     <form class="form-signin" _lpchecked="1">
-        <h2 class="form-signin-heading">Log alstublieft in.</h2>
+        <center><h2 class="form-signin-heading">Log alstublieft in</h2></center>
         <label for="inputEmail" class="sr-only">Gebruikersnaam</label>
         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;" autocomplete="off">
         <label for="inputPassword" class="sr-only">Wachtwoord</label>
