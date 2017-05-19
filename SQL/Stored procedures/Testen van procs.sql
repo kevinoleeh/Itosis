d716011a0d0471d881f915b9821d8e576a6ca599
Kevin Schomper <kevinaschomper@gmail.com>
2017-05-19 10:23:58 +0200

    Testen van procs eind versie eerste iteratie en test procs geupdate

--- SQL/Stored procedures/Testen van procs.sql
+++ SQL/Stored procedures/Testen van procs.sql
@@ -5,7 +5,7 @@ BEGIN TRY
 	BEGIN TRANSACTION test
 		EXEC SP_INSERT_PROJECT @Bedrijfsnaam = 'HAN', @Locatie = 'Arnhem', @ProjectOmschrijving = 'Project voor de han';
 	ROLLBACK TRANSACTION
-	EXEC _result 'SP_INSERT_PROJECT', 1, '', ''
+	EXEC _result 'SP_INSERT_PROJECT', 1, 'Project bij HAN Arnhem toegevoegd', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
@@ -37,7 +37,7 @@ BEGIN TRY
 	BEGIN TRANSACTION test
 		EXEC SP_INSERT_BEDRIJF @Bedrijfsnaam = 'HAN', @Locatie = 'Gelre'
 	ROLLBACK TRANSACTION
-	EXEC _result 'SP_INSERT_BEDRIJF', 1, '', ''
+	EXEC _result 'SP_INSERT_BEDRIJF', 1, 'Nieuw bedrijf van de HAN op andere locatie', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
@@ -53,7 +53,7 @@ BEGIN TRY
 	BEGIN TRANSACTION test
 		EXEC SP_DELETE_BEDRIJF @Bedrijfsnaam = 'HAN', @Locatie = 'Arnhem'
 	ROLLBACK TRANSACTION
-	EXEC _result 'SP_DELETE_BEDRIJF', 1, '', ''
+	EXEC _result 'SP_DELETE_BEDRIJF', 1, 'HAN Arnhem verwijdert', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
@@ -69,7 +69,7 @@ BEGIN TRY
 	BEGIN TRANSACTION test
 		EXEC SP_UPDATE_BEDRIJF @Bedrijfsnaam = 'HAN', @Locatie = 'Arnhem', @uBedrijfsnaam = 'HAN', @uLocatie =  'Nijmegen'
 	ROLLBACK TRANSACTION
-	EXEC _result 'SP_UPDATE_BEDRIJF', 1, '', ''
+	EXEC _result 'SP_UPDATE_BEDRIJF', 1, 'HAN Arnhem locatie verandert naar HAN Nijmegen', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
@@ -97,14 +97,14 @@ GO
 EXEC _begin
 BEGIN TRY
 	BEGIN TRANSACTION test
-		EXEC SP_UPDATE_BEDRIJF @Bedrijfsnaam = 'HAN', @Locatie = 'Arnhem', @uBedrijfsnaam = 'Hai', @uLocatie =  'Hai'
+		EXEC SP_UPDATE_BEDRIJF @Bedrijfsnaam = 'HAN', @Locatie = 'Arnhem', @uBedrijfsnaam = 'Euratex', @uLocatie =  'Duiven'
 	ROLLBACK TRANSACTION
 	EXEC _result 'SP_UPDATE_BEDRIJF', 0, '', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
 	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
-	EXEC _result 'SP_UPDATE_BEDRIJF', 1, 'Bedrijf met de naam Hai bestond al', @msg
+	EXEC _result 'SP_UPDATE_BEDRIJF', 1, 'Bedrijf met de naam Euratex bestaat al', @msg
 END CATCH
 EXEC _end 0
 GO
@@ -128,10 +128,10 @@ GO
 EXEC _begin
 BEGIN TRY
 	BEGIN TRANSACTION test
-		DECLARE @nmr INT = (SELECT projectnummer FROM PROJECT WHERE BEDRIJFSNAAM = 'EURATEX' AND LOCATIE = 'Duiven')
+		DECLARE @nmr INT = (SELECT projectnummer FROM PROJECT WHERE BEDRIJFSNAAM = 'EURATEX' AND LOCATIE = 'Duiven' AND PROJECTOMSCHRIJVING = 'TEST2')
 		EXEC SP_DELETE_PROJECT @nmr
 	ROLLBACK TRANSACTION
-	EXEC _result 'SP_DELETE_PROJECT', 1, '', ''
+	EXEC _result 'SP_DELETE_PROJECT', 1, 'Project verwijderd zonder rapporten', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
@@ -145,10 +145,10 @@ GO
 EXEC _begin
 BEGIN TRY
 	BEGIN TRANSACTION test
-		DECLARE @nmr INT = (SELECT projectnummer FROM PROJECT WHERE BEDRIJFSNAAM = 'EURATEX' AND LOCATIE = 'Duiven')
+		DECLARE @nmr INT = (SELECT projectnummer FROM PROJECT WHERE BEDRIJFSNAAM = 'EURATEX' AND LOCATIE = 'Duiven' AND PROJECTOMSCHRIJVING = 'Test')
 		EXEC SP_UPDATE_PROJECT @nmr, 'nieuwe omschrijving'
 	ROLLBACK TRANSACTION
-	EXEC _result 'SP_UPDATE_PROJECT', 1, '', ''
+	EXEC _result 'SP_UPDATE_PROJECT', 1, 'Nieuwe omschrijving voor project met rapporten', ''
 END TRY
 BEGIN CATCH
 	ROLLBACK TRANSACTION
@@ -169,6 +169,231 @@ BEGIN CATCH
 	ROLLBACK TRANSACTION
 	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
 	EXEC _result 'SP_UPDATE_PROJECT', 0, '', @msg
+END CATCH
+EXEC _end 0
+GO
+-- Update organisatie risicoregel
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		DECLARE @projectnummer INT = (SELECT projectnummer FROM PROJECT WHERE BEDRIJFSNAAM = 'EURATEX' AND LOCATIE = 'Duiven' AND PROJECTOMSCHRIJVING = 'Test')
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			@projectnummer,
+			1,
+			1,
+			'Beverages',
+			'Add. Words',
+			'Nieuw test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Standaard succes update van organisatie risicoregel', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, '', @msg
+END CATCH
+EXEC _end 0
+GO
+
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			99999,
+			1,
+			1,
+			'Test 1',
+			'Test 1',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, 'Projectnummer bestaat niet.', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Projectnummer bestaat niet.', @msg
+END CATCH
+EXEC _end 0
+GO
+
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			1,
+			99999,
+			1,
+			'Test 1',
+			'Test 1',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, 'Rapportnummer bestaat niet.', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Rapportnummer bestaat niet.', @msg
+END CATCH
+EXEC _end 0
+GO
+
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			1,
+			1,
+			99999,
+			'Test 1',
+			'Test 1',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, 'Rapportnummer bestaat niet.', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Rapportnummer bestaat niet.', @msg
+END CATCH
+EXEC _end 0
+GO
+
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			1,
+			1,
+			1,
+			'Bestaat niet',
+			'Test 1',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, 'Aspect bestaat niet.', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Aspect bestaat niet.', @msg
+END CATCH
+EXEC _end 0
+GO
+
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			1,
+			1,
+			1,
+			'Test 1',
+			'Bestaat niet',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, 'Effect bestaat niet.', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Effect bestaat niet.', @msg
+END CATCH
+EXEC _end 0
+GO
+
+EXEC _begin
+BEGIN TRY
+	BEGIN TRANSACTION test
+		EXEC SP_UPDATE_ORGANISATIE_RISICOREGEL
+			1,
+			1,
+			1,
+			'Beverages',
+			'Internet',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			'Test',
+			1,
+			1,
+			1,
+			1,
+			1,
+			1
+	ROLLBACK TRANSACTION
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 0, 'Aspect hoort niet bij effect.', ''
+END TRY
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+	DECLARE @msg VARCHAR(200) = ERROR_MESSAGE()
+	EXEC _result 'SP_UPDATE_ORGANISATIE_RISICOREGEL', 1, 'Aspect hoort niet bij effect.', @msg
 END CATCH
 EXEC _end 1
 GO
