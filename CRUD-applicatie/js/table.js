f5c978220b1cb48f57165d81931790791b1a1b7f
Ali Yilmaz <aliyilmazgc@gmail.com>
2017-05-23 13:39:07 +0200

    table.js klaargemaakt voor implementatie van regel openen dmv een knop ipv op de nummer in de row klikken.

--- CRUD-applicatie/js/table.js
+++ CRUD-applicatie/js/table.js
@@ -1,10 +1,11 @@
-$("#table tbody tr").click(function(){
+$("c").click(function(){
     $(this).addClass('info').siblings().removeClass('info');
 
     //dit is de waarde die gehaald wordt uit de geselecteerde row, in dit geval de eerste kolom; regelnummer
     var regelnummer=$(this).find('td:first').html();
 
     document.getElementById("pvabutton").href = "c_plan_van_aanpak.php?projectnummer="+projectnummer+"&rapportnummer="+rapportnummer+"&regelnummer="+regelnummer;
+    document.getElementById("regelopenenbutton").href = "u_organisatie_risicoregel.php?projectnummer="+projectnummer+"&rapportnummer="+rapportnummer+"&regelnummer="+regelnummer;
 
 
 });
