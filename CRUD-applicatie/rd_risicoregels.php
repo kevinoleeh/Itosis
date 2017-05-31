117896491d8817aaaf6f21fc1f3a7662167748c2
Kevin Schomper <kevinaschomper@gmail.com>
2017-05-31 10:46:40 +0200

    commit commit

--- CRUD-applicatie/rd_risicoregels.php
+++ CRUD-applicatie/rd_risicoregels.php
@@ -1,23 +1,36 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    session_start();
   error_reporting(E_ALL); ini_set('display_errors', TRUE); ini_set('display_startup_errors', TRUE);
   session_start();
   include_once('PHPExcel/Classes/PHPExcel.php');
   include_once('include/pdo-connect.php');
   $query = "SELECT *
-            FROM RISICOREGEL
-            WHERE PROJECTNUMMER = :PROJECTNUMMER
-            AND RAPPORTNUMMER = :RAPPORTNUMMER";
+            FROM RISICOREGEL r
+            LEFT JOIN VISUELE_BEOORDELING vb ON r.PROJECTNUMMER = vb.PROJECTNUMMER  AND r.RAPPORTNUMMER = vb.RAPPORTNUMMER   AND r.REGELNUMMER = vb.REGELNUMMER
+            LEFT JOIN MACHINE_VEILIGHEID m ON r.PROJECTNUMMER = m.PROJECTNUMMER AND r.RAPPORTNUMMER = m.RAPPORTNUMMER AND r.REGELNUMMER = m.REGELNUMMER
+            WHERE r.PROJECTNUMMER = :PROJECTNUMMER
+            AND r.RAPPORTNUMMER = :RAPPORTNUMMER";
+  $query2 = "SELECT REGELNUMMER
+             FROM RISICOREGEL
+             WHERE PROJECTNUMMER = :PROJECTNUMMER
+             AND RAPPORTNUMMER = :RAPPORTNUMMER";
   $stmt = $dbh->prepare($query);
   $projectnummer = $_GET['projectnummer'];
   $rapportnummer = $_GET['rapportnummer'];
+  $stmt2 = $dbh->prepare($query2);
+  $stmt2->bindParam(':PROJECTNUMMER', $projectnummer);
+  $stmt2->bindParam(':RAPPORTNUMMER', $rapportnummer);
+  $stmt2->execute();
   $stmt->bindParam(':PROJECTNUMMER', $projectnummer);
   $stmt->bindParam(':RAPPORTNUMMER', $rapportnummer);
   $stmt->execute();
   $results = array();
+  $results2 = array();
   while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
     $results[] = $row;
+  }
+  while($row = $stmt2->fetch(PDO::FETCH_ASSOC)){
+    $results2[] = $row;
   }
   $filename =  'Projectnummer'.$projectnummer.'Rapportnummer'.$rapportnummer.' Excel';
   $objPHPExcel = new PHPExcel();
@@ -35,6 +48,7 @@ while ($excelLetter !== 'AAA') {
 
   $setHeaders = false;
   $keys = array_keys($results[0]);
+  $offset = '2';
   for($i = 0; $i < sizeof($keys); $i ++){
     $objPHPExcel->getActiveSheet()->getColumnDimension($excelLetters[$i])->setWidth(22);
   }
@@ -47,10 +61,21 @@ while ($excelLetter !== 'AAA') {
       $setHeaders = true;
     }
     for($i = 0; $i < sizeof($row); $i ++){
-      if($key == 0 or $key == 1){
-        $key = 2;
+      $offsetfinal = ''+intval($key)+intval($offset);
+      if($i < 2){
+        if($i < 1){
+          $objPHPExcel->getActiveSheet()->setCellValue($excelLetters[$i].$offsetfinal, $projectnummer);
+        }
+        else{
+          $objPHPExcel->getActiveSheet()->setCellValue($excelLetters[$i].$offsetfinal, $rapportnummer);
+        }
       }
-      $objPHPExcel->getActiveSheet()->setCellValue($excelLetters[$i].$key, $row[$keys[$i]]);
+      if($i == 2){
+        $objPHPExcel->getActiveSheet()->setCellValue($excelLetters[$i].$offsetfinal, $results2[$key]["REGELNUMMER"]);
+      }
+      if($i > 2){
+      $objPHPExcel->getActiveSheet()->setCellValue($excelLetters[$i].$offsetfinal, $row[$keys[$i]]);
+    }
     }
   }
 
@@ -63,8 +88,8 @@ while ($excelLetter !== 'AAA') {
 include_once('include/pdo-disconnect.php');
 exit();
 } else {?>
-<?php include_once('include/header.php'); ?>
-<?php
+    <?php include_once('include/header.php'); ?>
+    <?php
 
 $query = "SELECT *
           FROM RISICOREGEL
@@ -116,88 +141,101 @@ if ($type['RAPPORT_TYPE'] === 'Organisatie') {
     $url = 'organisatie_risicoregel.php';
 } else if ($type['RAPPORT_TYPE'] === 'Visuele beoordeling') {
     $url = 'visuele_beoordeling_risicoregel.php';
-} else if ($type['RAPPORT_TYPE'] === 'Machine veiligheid'){
+} else if ($type['RAPPORT_TYPE'] === 'Machineveiligheid'){
     $url = 'machineveiligheid_risicoregel.php';
 }
 
 ?>
 
-    <div class="container">
-        <div class="page-header">
-            <h1>Regels</h1>
-            <h4>Projectnummer <?= $_GET['projectnummer'] ?>, rapportnummer <?= $_GET['rapportnummer'] ?></h4>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <a id="regelopenenbutton" class="btn btn-block btn-primary">Regel openen</a>
+        <div class="container">
+            <div class="page-header">
+                <h1>Regels</h1>
+                <h4>Projectnummer
+                    <?= $_GET['projectnummer'] ?>, rapportnummer
+                        <?= $_GET['rapportnummer'] ?>
+                </h4>
             </div>
-            <div class="col-md-4">
-                <a href="risicoregel.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>"
-                   class="btn btn-block btn-primary">Regel toevoegen</a>
-            </div>
-            <div class="col-md-4">
-                <form action ="rd_risicoregels.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>" method ="post">
-                <input type="submit" class="btn btn-block btn-default" value="Regels exporteren naar Excel">
-              </div>
 
-            <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <a id="regelopenenbutton" class="btn btn-block btn-primary">Regel openen</a>
+                </div>
+                <div class="col-md-4">
+                    <a href="risicoregel.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>" class="btn btn-block btn-primary">Regel toevoegen</a>
+                </div>
+                <div class="col-md-4">
+                    <form action="rd_risicoregels.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>" method="post">
+                        <input type="submit" class="btn btn-block btn-default" value="Regels exporteren naar Excel">
+                </div>
 
-                <br>
+                <div class="col-md-12">
 
-                <table id="table" class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Regelnummer</th>
-                        <th>Arbo onderwerp</th>
-                        <th>Aspect</th>
-                        <th>Effect</th>
-                        <th>Risico voor maatregel</th>
-                        <th>Prioriteit voor maatregel</th>
-                        <th>Plan van aanpak</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($result as &$value) { ?>
-                        <tr>
-                            <td style="display: none;"><?= $value['REGELNUMMER'] ?></td>
-                            <td><?= $value['REGELNUMMER'] ?></td>
-                            <td><?= $value['ARBO_ONDERWERP'] ?></td>
-                            <td><?= $value['ASPECTNAAM'] ?></td>
-                            <td><?= $value['EFFECTNAAM'] ?></td>
-                            <td><?= $value['VOOR_RISICO'] ?></td>
-                            <td><?= $value['VOOR_PRIORITEIT'] ?></td>
-                            <td><?php if (in_array($value['REGELNUMMER'], $regelnummers)) { ?>
+                    <br>
+
+                    <table id="table" class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Regelnummer</th>
+                                <th>Arbo onderwerp</th>
+                                <th>Aspect</th>
+                                <th>Effect</th>
+                                <th>Risico voor maatregel</th>
+                                <th>Prioriteit voor maatregel</th>
+                                <th>Plan van aanpak</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($result as &$value) { ?>
+                            <tr>
+                                <td style="display: none;">
+                                    <?= $value['REGELNUMMER'] ?>
+                                </td>
+                                <td>
+                                    <?= $value['REGELNUMMER'] ?>
+                                </td>
+                                <td>
+                                    <?= $value['ARBO_ONDERWERP'] ?>
+                                </td>
+                                <td>
+                                    <?= $value['ASPECTNAAM'] ?>
+                                </td>
+                                <td>
+                                    <?= $value['EFFECTNAAM'] ?>
+                                </td>
+                                <td>
+                                    <?= $value['VOOR_RISICO'] ?>
+                                </td>
+                                <td>
+                                    <?= $value['VOOR_PRIORITEIT'] ?>
+                                </td>
+                                <td>
+                                    <?php if (in_array($value['REGELNUMMER'], $regelnummers)) { ?>
 
                                     <div class="">
-                                        <a id="pvabutton"
-                                           href="u_plan_van_aanpak.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>&regelnummer=<?= $value['REGELNUMMER'] ?>"
-                                           class="btn btn-block btn-primary">PvA wijzigen</a>
+                                        <a id="pvabutton" href="u_plan_van_aanpak.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>&regelnummer=<?= $value['REGELNUMMER'] ?>" class="btn btn-block btn-primary">PvA wijzigen</a>
                                     </div>
 
                                     <?php
                                 } else {
                                     ?>
-                                    <div class="">
-                                        <a id="pvabutton"
-                                           href="c_plan_van_aanpak.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>&regelnummer=<?= $value['REGELNUMMER'] ?>"=
-                                           class="btn btn-block btn-primary">PvA toevoegen</a>
-                                    </div>
-                                    <?php
-                                } ?></td>
-                        </tr>
-                    <?php } ?>
-                    </tbody>
-                </table>
+                                        <div class="">
+                                            <a id="pvabutton" href="c_plan_van_aanpak.php?projectnummer=<?= $_GET['projectnummer'] ?>&rapportnummer=<?= $_GET['rapportnummer'] ?>&regelnummer=<?= $value['REGELNUMMER'] ?>"=c lass="btn btn-block btn-primary">PvA toevoegen</a>
+                                        </div>
+                                        <?php
+                                } ?>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
-    </div>
-    <script type="text/javascript">
-        var projectnummer = "<?php echo $_GET['projectnummer']; ?>";
-        var rapportnummer = "<?php echo $_GET['rapportnummer']; ?>";
-
-    </script>
-<?php
+        <script type="text/javascript">
+            var projectnummer = "<?php echo $_GET['projectnummer']; ?>";
+            var rapportnummer = "<?php echo $_GET['rapportnummer']; ?>";
+        </script>
+        <?php
    include_once('include/footer.php');
  }
 ?>
