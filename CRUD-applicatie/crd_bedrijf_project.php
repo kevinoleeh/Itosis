439e36ed522eaf2555e23fb07714a653ac9e0bf4
Kevin Schomper <kevinaschomper@gmail.com>
2017-06-02 09:09:57 +0200

    Errors gefixt

--- CRUD-applicatie/crd_bedrijf_project.php
+++ CRUD-applicatie/crd_bedrijf_project.php
@@ -127,26 +127,45 @@ if(isset($_POST["omschrijvingNew"])){
       $melding = "Project niet geüpdate. Foutmelding: " . $e->getMessage();
   }
 }
+try{
+$rs = $dbh->query("SELECT * FROM BEDRIJF");
+$bedrijven = $rs->fetchAll();
+}
+catch (PDOException $e) {
+    $meldingStatus = false;
+    $melding = "Project niet geüpdate. Foutmelding: " . $e->getMessage();
+}
+if (isset($_GET['project'])) {
+try{
+$stmt = $dbh->prepare("SELECT * FROM PROJECT WHERE BEDRIJFSNAAM = :BEDRIJFSNAAM AND LOCATIE = :LOCATIE");
+$stmt->execute(array(':BEDRIJFSNAAM' => $_GET['project'], ':LOCATIE' => $_GET['locatie']));
+$projecten = $stmt->fetchAll();
+}
+catch (PDOException $e) {
+    $meldingStatus = false;
+    $melding = "Project niet geüpdate. Foutmelding: " . $e->getMessage();
+}
+}
 
 ?>
-<div class="container">
-    <div class="page-header">
-        <h1>Bedrijven en projecten beheren</h1>
-    </div>
-    <?php include_once('include/melding.php') ?>
+    <div class="container">
+        <div class="page-header">
+            <h1>Bedrijven en projecten beheren</h1>
+        </div>
+        <?php include_once('include/melding.php') ?>
 
-    <div class="row">
-        <div class="col-md-5">
-            <a href="?new=1" class="btn btn-block btn-primary">Bedrijf toevoegen</a>
-            <div class="table-responsive marginTop">
-                <table id="table" class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Bedrijf</th>
-                        <th>Locatie</th>
-                    </tr>
-                    </thead>
-                    <?php
+        <div class="row">
+            <div class="col-md-5">
+                <a href="?new=1" class="btn btn-block btn-primary">Bedrijf toevoegen</a>
+                <div class="table-responsive marginTop">
+                    <table id="table" class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Bedrijf</th>
+                                <th>Locatie</th>
+                            </tr>
+                        </thead>
+                        <?php
                     if (isset($_GET["new"])) {
 
                         echo '<form action="crd_bedrijf_project.php?new=1" method="post">';
@@ -167,9 +186,7 @@ if(isset($_POST["omschrijvingNew"])){
                         echo '</tr>';
                         echo '</form>';
                     }
-
-                    $rs = $dbh->query("SELECT * FROM BEDRIJF");
-                    $bedrijven = $rs->fetchAll();
+                    if(isset($bedrijven)){
                     foreach ($bedrijven as $bedrijf) {
                         echo '<tr>';
                         echo '<td><a class="no-link" href="?project=' . $bedrijf["BEDRIJFSNAAM"] . '&locatie=' . $bedrijf["LOCATIE"] . '">' . $bedrijf["BEDRIJFSNAAM"] . '</a></td>';
@@ -177,29 +194,30 @@ if(isset($_POST["omschrijvingNew"])){
                     <a href="?remove=' . $bedrijf["BEDRIJFSNAAM"] . '&LOCATIE=' . $bedrijf["LOCATIE"] . '"><span class="glyphicon glyphicon-remove widintable red"></span></a>
                     <a href="?editbedrijf=' . $bedrijf["BEDRIJFSNAAM"] . '&LOCATIE=' . $bedrijf["LOCATIE"] . '"><span class="glyphicon glyphicon-pencil widintable"></span></a>';
                     }
+                  }
                     ?>
-                </table>
+                    </table>
+                </div>
             </div>
-        </div>
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-5">
-            <?php if (isset($_GET['project'])) {
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-5">
+                <?php if (isset($_GET['project'])) {
                 echo '<a class="no-link" href="?newProject=1&project=' . $_GET['project'] . '&locatie=' . $_GET['locatie'] . '">
                   <button class="btn btn-block btn-primary">Project aan bedrijf toevoegen</button>
                   </a>';
             }
             ?>
-            <?php if (isset($_GET['project'])) { ?>
-            <div class="table-responsive marginTop">
-                <table id="table" class="table table-striped table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Project</th>
-                        <th>Omschrijving</th>
-                    </tr>
-                    </thead>
-                    <?php
+                <?php if (isset($_GET['project'])) { ?>
+                <div class="table-responsive marginTop">
+                    <table id="table" class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Project</th>
+                                <th>Omschrijving</th>
+                            </tr>
+                        </thead>
+                        <?php
                     if (isset($_GET["newProject"])) {
                         echo '<form action="crd_bedrijf_project.php?project=' . $_GET['project'] . '&locatie=' . $_GET['locatie'] . '" method="post">';
                         echo '<tr>';
@@ -221,9 +239,6 @@ if(isset($_POST["omschrijvingNew"])){
                         echo '</tr>';
                         echo '</form>';
                     }
-                    $stmt = $dbh->prepare("SELECT * FROM PROJECT WHERE BEDRIJFSNAAM = :BEDRIJFSNAAM AND LOCATIE = :LOCATIE");
-                    $stmt->execute(array(':BEDRIJFSNAAM' => $_GET['project'], ':LOCATIE' => $_GET['locatie']));
-                    $projecten = $stmt->fetchAll();
                     foreach ($projecten as $project) {
                         echo '<tr>';
                         echo '<td><a href=rd_rapportages.php?projectnummer=' . $project["PROJECTNUMMER"] . '>' . $project["PROJECTNUMMER"] . '</a></td>';
@@ -233,10 +248,10 @@ if(isset($_POST["omschrijvingNew"])){
                     }
                     }
                     ?>
-                </table>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<?php include_once('include/footer.php'); ?>
+    <?php include_once('include/footer.php'); ?>
